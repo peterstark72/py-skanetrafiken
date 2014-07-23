fdc2982c930753a4e5c1e5ca563f548d97d8d03a
peterstark72 <peterstark72@gmail.com>
2014-07-23 10:05:59 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='skanetrafiken',
+      version='1.0',
+      py_modules=['skanetrafiken'],
+      author='Peter Stark',
+      author_email='peterstark72@gmail.com',
+      url='https://github.com/peterstark72/skanetrafiken',
+      requires=['lxml'],
+      description="Python wrapper for Skanetrafiken Open API",
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: Free for non-commercial use',
+                   'Natural Language :: Swedish',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Internet']
+      )
